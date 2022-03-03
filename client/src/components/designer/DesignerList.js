9995c49bc5d1f9cff3f43bcec6f6404b94e79891
Mitanshu <mitanshu2531@gmail.com>
2022-03-04 01:03:57 +0530

    popup confirm delete added in designer

--- client/src/components/designer/DesignerList.js
+++ client/src/components/designer/DesignerList.js
@@ -10,6 +10,49 @@ function DesignerList({ designers, deletedesigner }) {
 
   return (
     <>
+      <div
+        className="modal"
+        id={`deleteModal${designers.id}`}
+        tabIndex="-1"
+        aria-labelledby={`deleteModalLabel${designers.id}`} //"deleteModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4
+                className="modal-title px-1"
+                id={`deleteModalLabel${designers.id}`}
+              >
+                Delete Designer?
+              </h4>
+            </div>
+            <div className="modal-body">
+              <div className="container">
+                <h5>
+                  Are you sure want to delete {'"'}
+                  {designers.firstname} {designers.lastname}
+                  {'"'}?
+                </h5>
+                <button
+                  data-bs-dismiss="modal"
+                  className="btn btn-primary mx-3 "
+                >
+                  Cancel
+                </button>
+                <button
+                  className="btn btn-danger mx-3"
+                  data-bs-dismiss="modal"
+                  onClick={() => deletedesigner(designers.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="col-3" onClick={redirectToDesigner}>
         <p>
           {designers.firstname} {designers.lastname}
@@ -19,10 +62,12 @@ function DesignerList({ designers, deletedesigner }) {
       <div className="col-4">{designers.designeremail}</div>
       <div className="col-2">
         <button
+          type="button"
           className="btn btn-danger"
-          onClick={() => deletedesigner(designers.id)}
+          data-bs-toggle="modal"
+          data-bs-target={`#deleteModal${designers.id}`}
         >
-          delete
+          Delete
         </button>
       </div>
     </>
